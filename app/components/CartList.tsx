be580fcc3a669b40a6d9a2e713b1fa5651cf3628
Francesco Sacchi <depsir@gmail.com>
2023-11-30 17:18:38 +0100

    better cartlist with line total

--- app/components/CartList.tsx
+++ app/components/CartList.tsx
@@ -3,11 +3,25 @@ import useCart from "~/hooks/useCart";
 
 const CartList = () => {
     const {cart } = useCart()
+    const line = (first: string, second: string) => {
+        return <div className={"flex justify-between"}>
+            <span>{first}</span>
+            <span>{second}</span>
+        </div>
+    }
+
+    const compute = (price:number, quantity:number) => {
+        return (((price*100) * (quantity*100))/10000).toFixed(2)
+
+    }
     return (
         <div>
-            <ul>
-                {cart.map((item: any) => <li>{item.name} - {item.price} - {item.quantity}</li>)}
-            </ul>
+            {cart.map((item: any) => {
+                if (item.quantity>1) {
+                    return [line(`${item.quantity} x ${item.price}`, ""), line(item.name, compute(item.price , item.quantity))]
+                } else
+                return line(item.name, item.price);
+            })}
         </div>
     )
 
