deffe4b32f56943304836002f30c034a4ff0e42a
Francesco Sacchi <depsir@gmail.com>
2025-03-24 11:09:39 +0100

    add NumberPad component stories to Storybook

---
+++ stories/NumberPad.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import NumberPad from '../app/components/NumberPad';
+
+const meta = {
+    title: 'Components/NumberPad',
+    component: NumberPad,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof NumberPad>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        onDigit: (digit) => { console.log('Digit pressed:', digit) },
+        onBackspace: () => { console.log('Backspace pressed') },
+    },
+};
+
+export const WithActions: Story = {
+    args: {
+        onDigit: (digit) => { alert(`Digit pressed: ${digit}`) },
+        onBackspace: () => { alert('Backspace pressed') },
+    },
+}; 
