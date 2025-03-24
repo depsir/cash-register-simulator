5ec0599f3a0ff948801875823449f8a1b09636e4
Francesco Sacchi <depsir@gmail.com>
2025-03-24 12:06:22 +0100

    add Keyboard component stories to Storybook

---
+++ stories/Keyboard.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Keyboard from '../app/components/Keyboard';
+
+const meta: Meta<typeof Keyboard> = {
+  title: 'Components/Keyboard',
+  component: Keyboard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    onDigit: (digit: string) => console.log(`Digit pressed: ${digit}`),
+    onBackspace: () => console.log('Backspace pressed'),
+  },
+};
+
+export const WithAction: Story = {
+  args: {
+    onDigit: (digit: string) => alert(`Digit pressed: ${digit}`),
+    onBackspace: () => alert('Backspace pressed'),
+  },
+};
