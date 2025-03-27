59aa6feb014c2148d1a114136621de66dee49a71
Francesco Sacchi <depsir@gmail.com>
2025-03-27 14:35:44 +0100

    feat: refactor Bar component to integrate Menu and enhance checkout options with ReceiveMessagesWrapper

--- app/routes/bar.tsx
+++ app/routes/bar.tsx
@@ -1,65 +1,94 @@
-import React from "react";
-import CartList from "~/components/CartList";
-import MultiElementTextBox from "~/components/ui/MultiElementTextBox";
-import Button from "~/components/ui/Button";
-import useCart from "~/hooks/useCart";
-import {useNavigate} from '@remix-run/react'
-import PaymentCash from "~/components/PaymentCash";
+import React from 'react';
+import CartList from '~/components/CartList';
+import MultiElementTextBox from '~/components/ui/MultiElementTextBox';
+import useCart from '~/hooks/useCart';
+import { useNavigate } from '@remix-run/react';
+import PaymentCash from '~/components/PaymentCash';
+import Menu from '~/components/ui/Menu';
+import ReceiveMessagesWrapper from '~/components/cashlessPayment/ReceiveMessagesWrapper';
 
-const bar = () => {
-    const {addManualPrice, total, emptyCart} = useCart([])
-    const navigate = useNavigate();
-    const [subpage, setSubpage] = React.useState("")
+const Bar: React.FC = () => {
+  const { addManualPrice, total, emptyCart } = useCart([]);
+  const navigate = useNavigate();
 
-    const onCheckout = async () => {
-        emptyCart();
-        setSubpage("")
-    }
+  const onCheckout = async () => {
+    emptyCart();
+  };
 
-    return (
-        <div className={"grid lg:grid-cols-[1fr_600px] grid-cols-[1fr_1fr] gap-2 w-full h-full"}>
-            <div className={"flex flex-col h-full"}>
-                <div className={"bg-white flex-1 p-2 drop-shadow overflow-auto basis-0 "}>
-                    <CartList/>
-                </div>
-                <div className={"flex-none mt-2"}>
-                    <MultiElementTextBox>
-                        <div>Totale</div>
-                        <div>{total.toFixed(2)}</div>
-                    </MultiElementTextBox>
-                </div>
-            </div>
-            <div className={"flex flex-col h-full overflow-auto"}>
-                {(!subpage) && <>
-                    <div className="flex flex-wrap">
-                        <Button onClick={() => addManualPrice(1, "caffe")}>caffe</Button>
-                        <Button onClick={() => addManualPrice(1.8, "cappuccino")}>cappuccino</Button>
-                        <Button onClick={() => addManualPrice(1.5, "brioche")}>brioche</Button>
-                        <Button onClick={() => addManualPrice(2, "ginseng")}>ginseng</Button>
-                        <Button onClick={() => addManualPrice(2.5, "cioccolata")}>cioccolata</Button>
-                        <Button onClick={() => addManualPrice(2.5, "the")}>the</Button>
-                        <Button onClick={() => addManualPrice(2, "caffe speciale")}>caffe speciale</Button>
-                        <Button onClick={() => addManualPrice(3, "succo")}>succo</Button>
-                        <Button onClick={() => addManualPrice(1, "acqua")}>acqua</Button>
-                        <Button onClick={() => addManualPrice(2, "bibita")}>bibita</Button>
-                        <Button onClick={() => addManualPrice(3.5, "torta")}>torta</Button>
-                        <Button onClick={() => addManualPrice(5, "panino")}>panino</Button>
-                        <Button onClick={() => addManualPrice(1.8, "latte")}>latte</Button>
-                        <Button onClick={() => addManualPrice(2.8, "biscotti")}>biscotti</Button>
-                    </div>
-                    <Button onClick={() => setSubpage("checkout")} icon={"checkout"}>checkout</Button>
-                    <Button onClick={() => navigate("/admin", { state: { from: "/bar" } })} icon={"settings"}>admin</Button>
-                </>}
-                {subpage == "checkout" && <>
-                    <Button onClick={() => setSubpage("payment-cash")} icon={"payment-cash"}>contanti</Button>
-                    <Button onClick={() => setSubpage("")} icon={"back"}>indietro</Button>
-                </>}
-                {subpage == "payment-cash" && <>
-                    <PaymentCash onEnter={onCheckout} onClear={() => setSubpage("")}/>
-                </>}
-            </div>
+  const menuConfig = [
+     {
+      label: 'Bevande Calde',
+      icon: 'hot-drink',
+      children: [
+        { label: 'Caffè', action: () => addManualPrice(1, 'Caffè') },
+        { label: 'Cappuccino', action: () => addManualPrice(1.8, 'Cappuccino') },
+        { label: 'Ginseng', action: () => addManualPrice(2, 'Ginseng') },
+        { label: 'Cioccolata', action: () => addManualPrice(2.5, 'Cioccolata') },
+        { label: 'Tè', action: () => addManualPrice(2.5, 'Tè') },
+        { label: 'Caffè Speciale', action: () => addManualPrice(2, 'Caffè Speciale') },
+        { label: 'Latte', action: () => addManualPrice(1.8, 'Latte') },
+      ],
+    },
+    {
+      label: 'Bevande Fredde',
+      icon: 'cold-drink',
+      children: [
+        { label: 'Succo', action: () => addManualPrice(3, 'Succo') },
+        { label: 'Acqua', action: () => addManualPrice(1, 'Acqua') },
+        { label: 'Bibita', action: () => addManualPrice(2, 'Bibita') },
+        { label: 'Latte', action: () => addManualPrice(1.8, 'Latte') },
+      ],
+    },
+    {
+      label: 'Cibo',
+      icon: 'food',
+      children: [
+        { label: 'Brioche', action: () => addManualPrice(1.5, 'Brioche') },
+        { label: 'Torta', action: () => addManualPrice(3.5, 'Torta') },
+        { label: 'Panino', action: () => addManualPrice(5, 'Panino') },
+        { label: 'Biscotti', action: () => addManualPrice(2.8, 'Biscotti') },
+      ],
+    },   {
+      label: 'Checkout',
+      icon: 'checkout',
+      children: [
+        {
+            label: 'Contanti',
+            icon: 'payment-cash',
+            component: (props) => <PaymentCash {...props} onClear={props.onBack} onEnter={onCheckout} />,
+        },
+        {
+            label: 'Cellulare',
+            icon: 'payment-cash',
+            component: (props) => <ReceiveMessagesWrapper {...props} amount={total} onClear={props.onBack} onEnter={onCheckout} />,
+        },      ],
+    },
+    {
+      label: 'Admin',
+      icon: 'settings',
+      action: () => navigate('/admin', { state: { from: '/bar' } }),
+    },
+
+  ];
+
+  return (
+    <div className="grid lg:grid-cols-[1fr_600px] grid-cols-[1fr_1fr] gap-2 w-full h-full">
+      <div className="flex flex-col h-full">
+        <div className="bg-white flex-1 p-2 drop-shadow overflow-auto basis-0">
+          <CartList />
         </div>
-    )
-}
+        <div className="flex-none mt-2">
+          <MultiElementTextBox>
+            <div>Totale</div>
+            <div>{total.toFixed(2)}</div>
+          </MultiElementTextBox>
+        </div>
+      </div>
+      <div className="flex flex-col h-full overflow-auto">
+        <Menu config={menuConfig} />
+      </div>
+    </div>
+  );
+};
 
-export default bar 
+export default Bar;
